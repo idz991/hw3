315184d05a013a80df9c69d5a7f07c78292e1898
idz991 <fidzik@gmx.de>
2017-06-20 22:23:01 +0200

    ...

--- html/main/login.php
+++ html/main/login.php
@@ -4,5 +4,10 @@
  </head>
  <body>
  <?php echo '<p>Hallo Welt</p>'; ?>
+ <? $pdo = new PDO('mysql:host=localhost;dbname=hw_users', 'root', 'babba2');
+ include 'password.php';
+$showFormular = true; 
+
+ ?>
  </body>
 </html>
