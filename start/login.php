89b124b7b30cf7ced55cee4d64ad686072f43067
idz991 <fidzik@gmx.de>
2017-06-24 17:57:18 +0200

    login.php #5

--- start/login.php
+++ start/login.php
@@ -1,13 +1,22 @@
 <?php
 $pdo = new PDO('mysql:host=localhost;dbname=hw_users', 'root', 'babba2');
 
-
-$sql = "DELETE FROM credentials WHERE passwort = :babba";
+$sqlselect = 'SELECT * FROM users';
+foreach($db->query($sqlselect) as $row) {
+      echo $row['id']."<br />";
+      echo $row['name']."<br />";
+      echo $row['pw']."<br /><br />";
+}
+/*
+$delete  = babba;
+$sql = "DELETE FROM credentials WHERE passwort = :delete";
+$queray = $db->prepare($sql);
+$
 if ($pdo->query($sql) === TRUE) {
   echo "qry successful";
 }
 else {
   echo "qry error";
 }
-
+*/
 ?>
