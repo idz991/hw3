59ad33c839112ebd8a73e7b864ae7f6b72b701a9
idz991 <fidzik@gmx.de>
2017-06-24 18:01:51 +0200

    ...

--- start/login.php
+++ start/login.php
@@ -2,7 +2,7 @@
 $pdo = new PDO('mysql:host=localhost;dbname=hw_users', 'root', 'babba2');
 echo "hello";
 
-foreach($db->query('SELECT * FROM users') as $row) {
+foreach($db->query('SELECT * FROM `users` WHERE 1') as $row) {
       echo $row['id']."<br />";
       echo $row['name']."<br />";
       echo $row['pw']."<br /><br />";
