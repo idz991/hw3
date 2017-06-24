1b85cb2460800a5ad6231f1b9efa2fb07f88fad9
idz991 <fidzik@gmx.de>
2017-06-24 17:28:47 +0200

    login.php #2

--- start/login.php
+++ start/login.php
@@ -1,16 +1,7 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=hw_users', 'root', 'babba2');
-  echo "Connection successfull";
-if ($pdo != null ) {
-  echo "Connection successfull";
-}
+//$pdo = new PDO('mysql:host=localhost;dbname=hw_users', 'root', 'babba2');
 
-else {
-  echo "Connection failed" ;
-}
-$sql = "SELECT * FROM credentials";
-foreach ($pdo->query($sql) as $row) {
-   echo $row['mail']."<br />";
-   echo $row['passwort']."<br />";
-}
+
+echo "Connection successfull";
+
 ?>
