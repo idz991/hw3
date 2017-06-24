8ae4b2e765d094477c2f4c87d7f1ce0eb35abae0
idz991 <fidzik@gmx.de>
2017-06-24 17:36:18 +0200

    test.php

--- start/login.php
+++ start/login.php
@@ -1,13 +1,7 @@
-
-//$pdo = new PDO('mysql:host=localhost;dbname=hw_users', 'root', 'babba2');
-
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
-echo "My first PHP script!";
-?>
+$pdo = new PDO('mysql:host=localhost;dbname=hw_users', 'root', 'babba2');
 
-</body>
-</html>
+
+echo "Connection successfull";
+
+?>
