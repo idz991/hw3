2d52aa067e220498ee8224e23fd208b271423a0f
idz991 <fidzik@gmx.de>
2017-06-24 17:43:24 +0200

    login.php #4

--- start/login.php
+++ start/login.php
@@ -2,7 +2,7 @@
 $pdo = new PDO('mysql:host=localhost;dbname=hw_users', 'root', 'babba2');
 
 
-$sql = "DELETE FROM credentials WHERE mail = test@test.test";
+$sql = "DELETE FROM credentials WHERE mail = "test@test.test";
 if ($pdo->query($sql) === TRUE) {
   echo "qry successful";
 }
