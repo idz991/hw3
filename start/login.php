e40cb470d051583c4747e87612169ea7b931b18f
idz991 <fidzik@gmx.de>
2017-06-24 17:58:20 +0200

    login.php #5

--- start/login.php
+++ start/login.php
@@ -1,7 +1,7 @@
 <?php
 $pdo = new PDO('mysql:host=localhost;dbname=hw_users', 'root', 'babba2');
 
-$sqlselect = 'SELECT * FROM users';
+$sqlselect = "SELECT * FROM users";
 foreach($db->query($sqlselect) as $row) {
       echo $row['id']."<br />";
       echo $row['name']."<br />";
