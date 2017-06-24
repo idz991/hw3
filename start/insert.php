a76b39938a472435e67a9785ab565bc3185f3c83
idz991 <fidzik@gmx.de>
2017-06-24 18:31:14 +0200

    ...

---
+++ start/insert.php
@@ -0,0 +1,28 @@
+
+
+<?php
+echo "DB TEST";
+$pdo = new PDO('mysql:host=localhost;dbname=hw_users', 'root', 'babba2');
+echo "<br /> - after PDO";
+if ($pdo) {
+  echo "<br /> Connection succefully established";
+}
+else {
+  echo "<br /> Connection error";
+}
+
+
+
+$sql = "INSERT INTO `users`(`id`, `name`, `pw`) VALUES (\"1\", \"Raschid\", \"babba\")";
+
+
+$qry = $pdo->prepare($sql);
+$qry->execute();
+if ($qry->execute()) {
+  echo "<br /> qry successful - DELETE";
+}
+else {
+  echo "<br /> qry error";  echo $qry->errorInfo()[2];
+}
+
+?>
